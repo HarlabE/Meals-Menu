37b0d9dbb0e656a83599be424501f539ad2f9abe
HarlabE <olagunjuayodeleganiyu@gmail.com>
2025-10-07 18:20:34 +0100

    explicit animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,50 +5,81 @@ import 'package:fl_todo_app/screens/meals.dart';
 import 'package:fl_todo_app/widgets/category_grid_item.dart';
 import 'package:flutter/material.dart';
 
-class CategoryScreem extends StatelessWidget {
-  const CategoryScreem({
-    super.key,
-    
-    required this.availableMeals,
-  });
+class CategoryScreem extends StatefulWidget {
+  const CategoryScreem({super.key, required this.availableMeals});
 
-  
   final List<Meal> availableMeals;
+
+  @override
+  State<CategoryScreem> createState() => _CategoryScreemState();
+}
+
+class _CategoryScreemState extends State<CategoryScreem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void selectCategory(BuildContext context, CategorySet category) {
-    final filteredMeal = availableMeals
+    final filteredMeal = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => MealsScreen(
-          title: category.title,
-          meals: filteredMeal,
-
-        ),
+        builder: (context) =>
+            MealsScreen(title: category.title, meals: filteredMeal),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: EdgeInsets.all(10),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                selectCategory(context, category);
+              },
+            ),
+        ],
       ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              selectCategory(context, category);
-            },
+      builder: (context, child) => SlideTransition(
+        position: Tween(begin: Offset(0, 0.3), end: Offset(0, 0)).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.elasticInOut,
           ),
-      ],
+        ),
+
+        child: child,
+      ),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,60 +4,51 @@ import 'package:fl_todo_app/widgets/meal_item.dart';
 import 'package:flutter/material.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({
-    super.key,
-    this.title,
-    required this.meals,
-   
-  });
+  const MealsScreen({super.key, this.title, required this.meals});
 
   final String? title;
   final List<Meal> meals;
-  
 
   void selectMeal(BuildContext context, Meal meal) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (ctx) =>
-            MealDetails(meal: meal),
-      ),
-    );
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (ctx) => MealDetails(meal: meal)));
   }
 
   @override
   Widget build(BuildContext context) {
-    final content = meals.isEmpty
-        ? Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Uh oh... No meals here',
-                  style: Theme.of(
-                    context,
-                  ).textTheme.bodyLarge!.copyWith(color: Colors.black),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Try another Category",
-                  style: Theme.of(
-                    context,
-                  ).textTheme.bodyLarge!.copyWith(color: Colors.black),
-                ),
-              ],
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
             ),
-          )
-        : ListView.builder(
-            itemCount: meals.length,
-            itemBuilder: (context, index) {
-              return MealItem(
-                meal: meals[index],
-                chosenMeal: (context, meal) {
-                  selectMeal(context, meal);
-                },
-              );
-            },
-          );
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          chosenMeal: (context, meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
 
     if (title == null) {
       return content;
